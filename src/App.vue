ae4442226ddb1e18933eb39eeefa8417b385e19a
nan <2337286751@qq.com>
2022-08-14 13:12:51 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <h3 style="color: green">citest</h3>
-  <h3 style="color: green">rollback</h3>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
